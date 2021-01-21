d64ae6104229c5ae40e418fc32f7f25dac18b4e5
RobertoCZapata <robertozapataz25@gmail.com>
2021-01-20 19:19:55 -0500

    Update de challange

--- src/components/ItemCard/index.js
+++ src/components/ItemCard/index.js
@@ -21,92 +21,92 @@ import { useDispatch } from "react-redux";
 
 import { eraseItem } from "../../actions/itemListActions";
 
-const useStyles = makeStyles(theme => ({
-	card: {
-		maxWidth: 600,
-		marginBottom: "15px"
-	},
-	media: {
-		height: 0,
-		paddingTop: "56.25%" // 16:9
-	},
-	expand: {
-		transform: "rotate(0deg)",
-		marginLeft: "auto",
-		transition: theme.transitions.create("transform", {
-			duration: theme.transitions.duration.shortest
-		})
-	},
-	expandOpen: {
-		transform: "rotate(180deg)"
-	},
-	avatar: {
-		backgroundColor: red[500]
-	}
+const useStyles = makeStyles((theme) => ({
+  card: {
+    maxWidth: 600,
+    marginBottom: "15px",
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
 }));
 
 const ItemCard = ({ data = {} }) => {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	const [show, element] = useNearScreen();
+  const [show, element] = useNearScreen();
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const handleDelete = itemId => {
-		dispatch(eraseItem(itemId));
-	};
+  const handleDelete = (itemId) => {
+    dispatch(eraseItem(itemId));
+  };
 
-	return (
-		<article className="item-card-container" ref={element}>
-			{show && (
-				<Fade in={show} timeout={1000}>
-					<Card className={classes.card}>
-						<CardHeader
-							avatar={
-								<Avatar aria-label="recipe" className={classes.avatar}>
-									R
-								</Avatar>
-							}
-							title={<Link to={`/detail/${data.id}`}>{data.title}</Link>}
-							subheader={moment.unix(data.created).fromNow()}
-							action={
-								<IconButton
-									aria-label="delete"
-									onClick={() => handleDelete(data.id)}
-								>
-									<DeleteIcon />
-								</IconButton>
-							}
-						/>
+  return (
+    <article className="item-card-container" ref={element}>
+      {show && (
+        <Fade in={show} timeout={1000}>
+          <Card className={classes.card}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  R
+                </Avatar>
+              }
+              title={<Link to={`/detail/${data.id}`}>{data.title}</Link>}
+              subheader={moment.unix(data.created).fromNow()}
+              action={
+                <IconButton
+                  aria-label="delete"
+                  onClick={() => handleDelete(data.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              }
+            />
 
-						{data.thumbnail && data.thumbnail.startsWith("http") ? (
-							<CardMedia className={classes.media} image={data.thumbnail} />
-						) : null}
+            {data.thumbnail && data.thumbnail.startsWith("http") ? (
+              <CardMedia className={classes.media} image={data.thumbnail} />
+            ) : null}
 
-						<CardContent>
-							<Typography variant="body2" color="textSecondary" component="p">
-								Autor: {data.author}
-							</Typography>
-						</CardContent>
-						<CardActions>
-							<IconButton aria-label="add comments">
-								<CommentIcon /> Comments
-								{data.num_comments}
-							</IconButton>
-							<IconButton aria-label="read unread icon">
-								{data.visited ? (
-									<DoneAllIcon aria-label="read" />
-								) : (
-									<MarkunreadIcon aria-label="unread" />
-								)}
-							</IconButton>
-							VISITED
-						</CardActions>
-					</Card>
-				</Fade>
-			)}
-		</article>
-	);
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Autor: {data.author}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <IconButton aria-label="add comments">
+                <CommentIcon /> Comments
+                {data.num_comments}
+              </IconButton>
+              <IconButton aria-label="read unread icon">
+                {data.visited ? (
+                  <DoneAllIcon aria-label="read" />
+                ) : (
+                  <MarkunreadIcon aria-label="unread" />
+                )}
+              </IconButton>
+              VISITED
+            </CardActions>
+          </Card>
+        </Fade>
+      )}
+    </article>
+  );
 };
 
 export default ItemCard;
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -10,113 +10,113 @@ import SearchIcon from "@material-ui/icons/Search";
 import { useDispatch } from "react-redux";
 import { getItemsSearch } from "../../actions/searchActions";
 
-const useStyles = makeStyles(theme => ({
-	grow: {
-		flexGrow: 1
-	},
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
 
-	title: {
-		display: "none",
-		[theme.breakpoints.up("sm")]: {
-			display: "block"
-		}
-	},
-	search: {
-		position: "relative",
-		borderRadius: theme.shape.borderRadius,
-		backgroundColor: fade(theme.palette.common.white, 0.15),
-		"&:hover": {
-			backgroundColor: fade(theme.palette.common.white, 0.25)
-		},
-		marginRight: theme.spacing(2),
-		marginLeft: 0,
-		width: "100%",
-		[theme.breakpoints.up("sm")]: {
-			marginLeft: theme.spacing(3),
-			width: "auto"
-		}
-	},
-	searchIcon: {
-		width: theme.spacing(7),
-		height: "100%",
-		position: "absolute",
-		pointerEvents: "none",
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center"
-	},
-	inputRoot: {
-		color: "inherit"
-	},
-	inputInput: {
-		padding: theme.spacing(1, 1, 1, 7),
-		transition: theme.transitions.create("width"),
-		width: "100%",
-		[theme.breakpoints.up("md")]: {
-			width: 200
-		}
-	},
-	sectionDesktop: {
-		display: "none",
-		[theme.breakpoints.up("md")]: {
-			display: "flex"
-		}
-	},
-	sectionMobile: {
-		display: "flex",
-		[theme.breakpoints.up("md")]: {
-			display: "none"
-		}
-	}
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200,
+    },
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
 }));
 
 const PrimarySearchAppBar = () => {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const searchItems = query => dispatch(getItemsSearch(query));
+  const searchItems = (query) => dispatch(getItemsSearch(query));
 
-	const handleChange = e => {
-		searchItems(e.target.value);
-	};
+  const handleChange = (e) => {
+    searchItems(e.target.value);
+  };
 
-	return (
-		<div className={classes.grow}>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton
-						edge="start"
-						className={classes.menuButton}
-						color="inherit"
-						aria-label="open drawer"
-					></IconButton>
-					<Typography className={classes.title} variant="h6" noWrap>
-						<a className="decoration-link" href="/">
-							Deviget-Challenge
-						</a>
-					</Typography>
-					<div className={classes.search}>
-						<div className={classes.searchIcon}>
-							<SearchIcon />
-						</div>
-						<InputBase
-							placeholder="Search…"
-							classes={{
-								root: classes.inputRoot,
-								input: classes.inputInput
-							}}
-							inputProps={{ "aria-label": "search" }}
-							onChange={handleChange}
-						/>
-					</div>
-					<div className={classes.grow} />
-					<div className={classes.sectionDesktop}></div>
-					<div className={classes.sectionMobile}></div>
-				</Toolbar>
-			</AppBar>
-		</div>
-	);
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          ></IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            <a className="decoration-link" href="/">
+              Deviget-Challenge
+            </a>
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}></div>
+          <div className={classes.sectionMobile}></div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default PrimarySearchAppBar;
